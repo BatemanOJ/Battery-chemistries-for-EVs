7e4895899c203711ef1ebd1e7cd50060f44c5927
BatemanOJ <ollie.j.bateman@gmail.com>
2025-03-02 15:54:35 +0000

    02-03_Efficiency improved

--- Find_Power_Dense_Battery.py
+++ Find_Power_Dense_Battery.py
@@ -7,9 +7,29 @@ def Find_Power_Dense_Battery(battery_1, battery_2):
     battery_1 = Get_Battery_Data_Row(battery_1)
     battery_2 = Get_Battery_Data_Row(battery_2)
 
+    
+
     battery_1_Wh = battery_1[14] * battery_1[16]
     battery_2_Wh = battery_2[14] * battery_2[16]
 
+    if battery_1_Wh < battery_2_Wh:
+        
+        battery_3 = battery_1
+        battery_1 = battery_2
+        battery_2 = battery_3
+        
+        battery_1_Wh = battery_1[14] * battery_1[16]
+        battery_2_Wh = battery_2[14] * battery_2[16]
+
+    return battery_1, battery_2, battery_1_Wh, battery_2_Wh
+
+def Find_Power_Dense_Battery_Efficient(battery_1, battery_2):
+
+    print(battery_1[14], battery_1[16])
+    
+    battery_1_Wh = battery_1[14] * battery_1[16]
+    battery_2_Wh = battery_2[14] * battery_2[16]
+    
     if battery_1_Wh < battery_2_Wh:
         
         battery_3 = battery_1
--- Main_Code_2.py
+++ Main_Code_2.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 import math
 import numpy as np
 
@@ -9,6 +9,7 @@ from Series_parallel_configuration import Series_Parallel_Config_EV
 from Range_Estimation import Range_Estimation_for_EVs
 from Get_Data_From_Cell import Get_Battery_Data_Row # gets the row of data and puts it into an array -2 on the row number
 from Two_Chemistries import Two_Chemistries
+from Two_Chem_Efficient import Two_Chem_Efficient
 
 
 # Load the Excel file
@@ -40,27 +41,68 @@ range_est_50kph, range_est_100kph = Range_Estimation_for_EVs(pack_capacity, EV_n
 
 # print(range_est_50kph, range_est_100kph)
 battery_1_index = 1
-battery_2_index = 97
+battery_2_index = 2
+
+# for i in range(1, 388):
+#     battery_{i}_index = Get_Battery_Data_Row(i)
+
+# battery_data = {f"battery_{i}_index": Get_Battery_Data_Row(i).tolist() for i in range(1, 388)}
+
+battery_data = {}
+
+battery_database = pd.read_excel("Battery database from open source_CellDatabase_v6.xlsx", sheet_name="RAW DATA")
+
+i = 1
+battery_data = {f"battery_{i}_index": battery_database.iloc[i].tolist() for i in range(388)}  # Adjusted for 387 rows
+
+print(battery_data[f"battery_1_index"][1])
+print(battery_data[f"battery_2_index"][1])
+print(battery_data[f"battery_3_index"][1])
+print(battery_data[f"battery_386_index"][1])
+print(battery_data[f"battery_387_index"][1])
+
+i = 1
 
 success, battery_1_series, battery_1_parallel, battery_2_series, battery_2_parallel = \
-    Two_Chemistries(battery_1_index, battery_2_index, 75000, 200000, 250, 475, 360, 318)
+    Two_Chem_Efficient(battery_data[f"battery_{battery_1_index}_index"], battery_data[f"battery_{battery_2_index}_index"], 75000, 200000, 475, 318)
 # print(f"Battery 1 Index: {battery_1_index} {battery_1_series}S {battery_1_parallel}P, Battery 2 Index: {battery_2_index} {battery_2_series}S {battery_2_parallel}P")
 
 while success == 0:
     if battery_2_index == 388:
         battery_1_index += 1
-        battery_2_index = battery_1_index
+        battery_2_index = battery_1_index + 1
     else:
         battery_2_index += 1
     
     success, battery_1_series, battery_1_parallel, battery_2_series, battery_2_parallel = \
-    Two_Chemistries(battery_1_index, battery_2_index, 75000, 200000, 250, 475, 360, 318)
+    Two_Chem_Efficient(battery_data[f"battery_{battery_1_index}_index"], battery_data[f"battery_{battery_2_index}_index"], 75000, 200000, 475, 318)
 
     print(f"Battery 1 Index: {battery_1_index} Battery 2 Index: {battery_2_index}")
     
     if success == 1:
         print(f"Battery 1 Index: {battery_1_index} {battery_1_series}S {battery_1_parallel}P, Battery 2 Index: {battery_2_index} {battery_2_series}S {battery_2_parallel}P")
         break
+
+
+# success, battery_1_series, battery_1_parallel, battery_2_series, battery_2_parallel = \
+#     Two_Chemistries(battery_1_index, battery_2_index, 75000, 200000, 250, 475, 360, 318)
+# # print(f"Battery 1 Index: {battery_1_index} {battery_1_series}S {battery_1_parallel}P, Battery 2 Index: {battery_2_index} {battery_2_series}S {battery_2_parallel}P")
+
+# while success == 0:
+#     if battery_2_index == 388:
+#         battery_1_index += 1
+#         battery_2_index = battery_1_index
+#     else:
+#         battery_2_index += 1
+    
+#     success, battery_1_series, battery_1_parallel, battery_2_series, battery_2_parallel = \
+#     Two_Chemistries(battery_1_index, battery_2_index, 75000, 200000, 250, 475, 360, 318)
+
+#     print(f"Battery 1 Index: {battery_1_index} Battery 2 Index: {battery_2_index}")
+    
+#     if success == 1:
+#         print(f"Battery 1 Index: {battery_1_index} {battery_1_series}S {battery_1_parallel}P, Battery 2 Index: {battery_2_index} {battery_2_series}S {battery_2_parallel}P")
+#         break
 
 # Two_Chemistries(required_cap, battery_1, battery_2, peak_power_required, min_pack_voltage, max_pack_voltage, pack_voltage, max_mass(kg))
 # print(min_battery_1_only, min_battery_2_only)
